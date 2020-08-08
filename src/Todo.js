cda7af59513d84a046c6ac229c682479ac70f7a3
Bdv <baltadava@gmail.com>
2020-08-07 23:39:48 -0300

    Update Todo.js

--- src/Todo.js
+++ src/Todo.js
@@ -1,18 +1,33 @@
 import React, { useState } from 'react';
-import { List, ListItem, ListItemText, ListItemAvatar, Modal, Button } from '@material-ui/core';
 import db from './firebase';
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import firebase from 'firebase';
+import { List, ListItem, ListItemText, ListItemAvatar, Modal, Button, Fab, Input } from '@material-ui/core';
+import EditIcon from '@material-ui/icons/Edit';
 import { makeStyles } from '@material-ui/core/styles';
+import DeleteIcon from '@material-ui/icons/Delete';
 import './Todo.css';
 
 const useStyles = makeStyles((theme) => ({
+    button: {
+        margin: theme.spacing(1),
+        float:"right",
+        position: 'absolute',
+        top: '-40px'
+      },
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+      },
     paper: {
       position: 'absolute',
-      width: 400,
+      width: '30%',
+      padding: 10,
       backgroundColor: theme.palette.background.paper,
       border: '2px solid #000',
       boxShadow: theme.shadows[5],
-      padding: theme.spacing(2, 4, 3) 
+      padding: theme.spacing(2, 4, 3)
     },
 }));
 
@@ -23,33 +38,55 @@ function Todo(props) {
 
     const updateTodo = (event) => {
         event.preventDefault();
-
-        //Update todo width new input text
+        
+        //Actualiza todo con los datos ingresados en el MODAL
         db.collection('todos').doc(props.todo.id).set({
-            todo: input
+            text: input,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
         }, { merge : true});
 
+        setInput('');
         setOpen(false);
     }
-            
+    
     return (
         <>
-        <Modal open={open} onClose={e => setOpen(false)}>
+        <Modal open={open} onClose={e => setOpen(false)} 
+        aria-labelledby="simple-modal-title" aria-describedby="simple-modal-description" 
+        className={classes.modal}>
             <div className={classes.paper}>
-                <h1>Modal</h1>
-                <input placeholder={props.todo.todo} value = {input} onChange={event => setInput(event.target.value)}/>
-                <Button onClick={updateTodo}>Update Todo</Button>
+                <h1 className="Modal__Title">Editar tarea</h1>
+                <form className="Modal__Form">
+                    <Input placeholder={props.todo.text} value = {input} 
+                    onChange={event => setInput(event.target.value)} 
+                    className="Modal__Input" />
+                    <Button disabled ={!input} 
+                    onClick={updateTodo} 
+                    className="Modal__Button"
+                    type="submit">Actualizar</Button>
+                </form>
             </div>
         </Modal>
-        <List >
+        <List className="List">
             <ListItem>
                 <ListItemAvatar> 
                 </ListItemAvatar>
-                <ListItemText primary={props.todo.todo} secondary='sdsd'/>
+                <ListItemText primary={props.todo.text} secondary='Date'/>
+                <Fab color="primary" aria-label="edit" size="small" 
+                className="List__Edit" 
+                style={{marginRight: 20}} >
+                    <EditIcon onClick={e => setOpen(true)}/>
+                </Fab>
+                <Button onClick={() => db.collection('todos').doc(props.todo.id).delete()} 
+                    variant="contained"
+                    color="secondary"
+                    className="List__Delete"
+                    style={{width : "10%"}}
+                    startIcon={<DeleteIcon style={{fontSize: "170%"}} />}> 
+                    Borrar</Button>
             </ListItem>
-            <DeleteForeverIcon onClick={() => db.collection('todos').doc(props.todo.id).delete()} className="todo__list delete"/>
-            <Button onClick={e => setOpen(true)} className="todo__list">Edit</Button >
         </List>
+        <br></br>
         </>
     )
 }
